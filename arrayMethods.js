f1972fd7f3bc236026933990d24a147dc8722f2d
naturalCode4 <naturalCode4@gmail.com>
2022-03-30 21:33:48 -0600

    first commit

---
+++ arrayMethods.js
@@ -0,0 +1,181 @@
+/*
+    You can check your answers in this file
+    by console.logging the variables that are 
+    storing the results of your function calls.
+*/
+
+////////// PROBLEM 1 //////////
+
+// Do not edit the code below.
+const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
+// Do not edit the code above.
+
+/*
+  Use the filter method on mixedNumbers to make a new array of just the even numbers.
+  The filter function takes a callback with the parameters
+  function(element, index, wholeArray){}  Function Form
+  (element, index, wholeArray)=>{}    Arrow Form
+*/
+
+// CODE HERE
+
+
+const evenNumbers = mixedNumbers.filter(num => num % 2 === 0)
+// console.log(evenNumbers)
+
+const getEven = num => num % 2 === 0
+const evenNumbers2 = mixedNumbers.filter(getEven)/* Provide Your Callback Here */
+// console.log(evenNumbers2)
+
+////////// PROBLEM 2 //////////
+
+// Do not edit the code below.
+const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
+// Do not edit the code above.
+
+/*
+  Use the map method on the prices array to calculate a new array of post-tax prices.
+  Use a 7% tax rate.
+  Math reminder! To calculate the price after tax, multiply the price by 1 plus the taxRate as a decimal.
+  Example: if tax is 7%, the price afterTax could be calculated like this: afterTax = price * 1.07) 
+  The map function also takes a callback with the parameters
+  function(element, index, wholeArray){}  Function Form
+  (element, index, wholeArray)=>{}    Arrow Form
+*/
+
+// CODE HERE
+
+const tax = initialPrice => initialPrice * 1.07
+const postTaxPrices = prices.map(tax);
+
+// console.log(postTaxPrices)
+
+
+
+////////// PROBLEM 3 //////////
+
+// Do not edit the code below.
+const populations = [8175133, 3792621, 2695598, 2100263];
+// Do not edit the code above.
+
+/*
+  Use the reduce method to calculate the sum of all the populations in the array.
+  The reduce function has a slightly different setup for its callback.
+  function(runningTotal, curElement, curIndex, wholeArray){} Function Form
+  (runningTotal, curElement, curIndex, wholeArray)=>{} Arrow Form
+*/
+
+// CODE HERE
+const populationCallback = (accumulatedVal, nextVal) => {
+  return accumulatedVal + nextVal}
+const totalPopulation = populations.reduce(populationCallback)
+// console.log(totalPopulation)
+
+
+
+////////// PROBLEM 4 //////////
+
+// Do not edit the code below.
+const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulbabunny","CP":135},
+{"monster":"Bulbabunny","CP":250},{"monster":"Ponylopse","CP":277},{"monster":"Ponylopse","CP":184},
+{"monster":"Pikadoughnet","CP":207},{"monster":"Bulbabunny","CP":139},{"monster":"Pikadoughnet","CP":47},
+{"monster":"Pikadoughnet","CP":175},{"monster":"WaterHorsia","CP":26},{"monster":"Ponylopse","CP":19},
+{"monster":"Pikadoughnet","CP":218},{"monster":"Charaflier","CP":101},{"monster":"WaterHorsia","CP":96},
+{"monster":"Pikadoughnet","CP":253},{"monster":"Sandmush","CP":146},{"monster":"Bulbabunny","CP":247},
+{"monster":"Charaflier","CP":55},{"monster":"Bulbabunny","CP":72},{"monster":"Pikadoughnet","CP":300},
+{"monster":"Sandmush","CP":262},{"monster":"Sandmush","CP":25},{"monster":"Charaflier","CP":215},
+{"monster":"Ponylopse","CP":125},{"monster":"Bulbabunny","CP":178}];
+// Do not edit the code above.
+
+/*
+  Now we're going to ramp these up a little bit.
+  Instead of just arrays of numbers, we are going to have an array of objects that we want to use map, filter, and reduce with.
+  Use the filter method to return only the monsters that have a CP of over 200.
+*/
+
+// CODE HERE
+
+// const filterStrong = (monster) => {
+//   return monster.CP > 200
+// }
+// const myStrong = monstersInYourPocket.filter(filterStrong)
+
+// // console.log(myStrong)
+
+//This code finds your strongest pokemon
+let currentStrongest = monstersInYourPocket[0]
+
+const findStrongestCallback = (monster) => {
+  if (monster.CP > currentStrongest.CP) {
+    currentStrongest = monster
+  }
+  return currentStrongest
+}
+
+const findStrongest = () => {
+  monstersInYourPocket.forEach(findStrongestCallback)
+  return 'Your strongest pokemon is ' + currentStrongest.monster + ' with a CP of ' + currentStrongest.CP
+}
+console.log(findStrongest())
+
+// /////
+// const each = (namesArr, cb) => {
+
+//   return namesArr.forEach((person, index) => cb(person))
+// }
+
+// each(uniqArrGlobal, function(person,index){
+//   console.log(`The item at index ${index} is ${person}.`)
+// })
+// ///////
+
+
+
+
+////////// PROBLEM 5 //////////
+
+// Do not edit code below.
+const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax":0.11},
+{"price":80,"tax":0.11},{"price":69,"tax":0.06},{"price":68,"tax":0.14},
+{"price":72,"tax":0.14},{"price":51,"tax":0.09},{"price":89,"tax":0.15},
+{"price":48,"tax":0.13}];
+// Do not edit code above.
+
+/*
+  Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
+*/
+
+// CODE HERE
+
+
+
+////////// PROBLEM 6 //////////
+
+// Do not edit the code below.
+const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
+{"owner":"Bob","price":73},{"owner":"Barry","price":57},{"owner":"Barry","price":128},
+{"owner":"Bob","price":119},{"owner":"Barry","price":133},{"owner":"Barry","price":27},
+{"owner":"Barry","price":138},{"owner":"Bob","price":68},{"owner":"Bob","price":50},
+{"owner":"Barry","price":9},{"owner":"Bob","price":123},{"owner":"Bob","price":135},
+{"owner":"Barry","price":30},{"owner":"Barry","price":129},{"owner":"Barry","price":38},
+{"owner":"Bob","price":133},{"owner":"Barry","price":109},{"owner":"Bob","price":115}];
+// Do not edit the code above.
+
+/*
+  Use a high order method(s) to get the sum of bobsTotal.
+*/
+
+// CODE HERE
+
+const bobsTotal = purchases.reduce((acc, purchase) => {
+  if(purchase.owner === 'Bob'){
+    return acc + purchase.price
+  } else return acc
+}, 0) //notice we are giving starting value of 0, or else it will take first value as starting value
+
+const bobsTotal2 = purchases.filter(purchase => purchase.owner === 'Bob').reduce((acc, purchase) => {
+  return acc + purchase.price
+}, 0)
+
+// console.log(bobsTotal)
+// console.log(bobsTotal2)
